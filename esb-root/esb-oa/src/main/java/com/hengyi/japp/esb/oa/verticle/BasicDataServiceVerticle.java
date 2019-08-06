fc3113b276644b5a945480bf5cdff132514e083a
jzb <ixtf1984@gmail.com>
2019-08-07 05:49:23 +0800

    add apm

--- esb-root/esb-oa/src/main/java/com/hengyi/japp/esb/oa/verticle/BasicDataServiceVerticle.java
+++ esb-root/esb-oa/src/main/java/com/hengyi/japp/esb/oa/verticle/BasicDataServiceVerticle.java
@@ -1,11 +1,15 @@
 package com.hengyi.japp.esb.oa.verticle;
 
 import com.hengyi.japp.esb.oa.soap.BasicDataService.BasicDataServicePortType;
+import io.opentracing.Span;
+import io.opentracing.Tracer;
 import io.reactivex.Completable;
 import io.reactivex.Single;
 import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.eventbus.MessageConsumer;
 import lombok.extern.slf4j.Slf4j;
 
+import static com.hengyi.japp.esb.core.Util.*;
 import static com.hengyi.japp.esb.oa.OaVerticle.OA_INJECTOR;
 
 /**
@@ -17,42 +21,63 @@ public class BasicDataServiceVerticle extends AbstractVerticle {
     @Override
     public Completable rxStart() {
         return Completable.mergeArray(
-                vertx.eventBus().<String>consumer("esb:oa:BasicDataService:getHrmresourceData", reply -> {
-                    final String body = reply.body();
-                    Single.fromCallable(() -> {
-                        final BasicDataServicePortType basicDataServicePortType = OA_INJECTOR.getInstance(BasicDataServicePortType.class);
-                        return basicDataServicePortType.getHrmresourceData("");
-                    }).subscribe(it -> {
-                        reply.reply(it);
-                    }, err -> {
-                        log.error("", err);
-                        reply.fail(400, err.getLocalizedMessage());
-                    });
-                }).rxCompletionHandler(),
-
-                vertx.eventBus().<String>consumer("esb:oa:BasicDataService:getDepartmentData", reply -> {
-                    Single.fromCallable(() -> {
-                        final BasicDataServicePortType basicDataServicePortType = OA_INJECTOR.getInstance(BasicDataServicePortType.class);
-                        return basicDataServicePortType.getDepartmentData("");
-                    }).subscribe(it -> {
-                        reply.reply(it);
-                    }, err -> {
-                        log.error("", err);
-                        reply.fail(400, err.getLocalizedMessage());
-                    });
-                }).rxCompletionHandler(),
-
-                vertx.eventBus().<String>consumer("esb:oa:BasicDataService:getSubcompanyData", reply -> {
-                    Single.fromCallable(() -> {
-                        final BasicDataServicePortType basicDataServicePortType = OA_INJECTOR.getInstance(BasicDataServicePortType.class);
-                        return basicDataServicePortType.getSubcompanyData("");
-                    }).subscribe(it -> {
-                        reply.reply(it);
-                    }, err -> {
-                        log.error("", err);
-                        reply.fail(400, err.getLocalizedMessage());
-                    });
-                }).rxCompletionHandler()
+                getHrmresourceData().rxCompletionHandler(),
+                getDepartmentData().rxCompletionHandler(),
+                getSubcompanyData().rxCompletionHandler()
         );
+    }
+
+    private MessageConsumer<String> getSubcompanyData() {
+        final String address = "esb:oa:BasicDataService:getSubcompanyData";
+        return vertx.eventBus().<String>consumer(address, reply -> {
+            final Tracer tracer = OA_INJECTOR.getInstance(Tracer.class);
+            final Span span = initApm(reply, tracer, this, "BasicDataService:getSubcompanyData", address);
+            Single.fromCallable(() -> {
+                final BasicDataServicePortType basicDataServicePortType = OA_INJECTOR.getInstance(BasicDataServicePortType.class);
+                return basicDataServicePortType.getSubcompanyData("");
+            }).subscribe(it -> {
+                apmSuccess(reply, span, it);
+                reply.reply(it);
+            }, err -> {
+                apmError(reply, span, err);
+                reply.fail(400, err.getLocalizedMessage());
+            });
+        });
+    }
+
+    private MessageConsumer<String> getDepartmentData() {
+        final String address = "esb:oa:BasicDataService:getDepartmentData";
+        return vertx.eventBus().<String>consumer(address, reply -> {
+            final Tracer tracer = OA_INJECTOR.getInstance(Tracer.class);
+            final Span span = initApm(reply, tracer, this, "BasicDataService:getDepartmentData", address);
+            Single.fromCallable(() -> {
+                final BasicDataServicePortType basicDataServicePortType = OA_INJECTOR.getInstance(BasicDataServicePortType.class);
+                return basicDataServicePortType.getDepartmentData("");
+            }).subscribe(it -> {
+                apmSuccess(reply, span, it);
+                reply.reply(it);
+            }, err -> {
+                apmError(reply, span, err);
+                reply.fail(400, err.getLocalizedMessage());
+            });
+        });
+    }
+
+    private MessageConsumer<String> getHrmresourceData() {
+        final String address = "esb:oa:BasicDataService:getHrmresourceData";
+        return vertx.eventBus().<String>consumer(address, reply -> {
+            final Tracer tracer = OA_INJECTOR.getInstance(Tracer.class);
+            final Span span = initApm(reply, tracer, this, "BasicDataService:getHrmresourceData", address);
+            Single.fromCallable(() -> {
+                final BasicDataServicePortType basicDataServicePortType = OA_INJECTOR.getInstance(BasicDataServicePortType.class);
+                return basicDataServicePortType.getHrmresourceData("");
+            }).subscribe(it -> {
+                apmSuccess(reply, span, it);
+                reply.reply(it);
+            }, err -> {
+                apmError(reply, span, err);
+                reply.fail(400, err.getLocalizedMessage());
+            });
+        });
     }
 }
